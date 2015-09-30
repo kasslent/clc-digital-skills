89dabd6bfe9c87d4daccb649f178e83772ea8a80
Christina Truong <hello@christinatruong.com>
2015-09-30 17:01:20 -0400

    Update site urls for home page.

---
+++ _site/assets/js/custom.js
@@ -0,0 +1,48 @@
+$(function(){
+  
+  // Adds current year to copyright date
+	var currentYear = (new Date()).getFullYear();
+	$("[data-year]").text(currentYear);
+  
+  
+  // Table of Contents
+  var ToC = "<p>On this page</p><ul>";
+  var newLine, el, title, link;
+
+  $("main h2").each(function() {
+    el = $(this);
+    title = el.text();
+    link = "#" + el.attr("id");
+
+    newLine =
+      "<li>" +
+        "<a href='" + link + "'>" +
+          title +
+        "</a>" +
+      "</li>";
+
+    ToC += newLine;
+  });
+  
+  ToC +="</ul>";
+  $(".table-of-contents").append(ToC);
+  
+  
+  var waypoints = $('[data-nav]').waypoint({
+    handler: function(direction) {
+      if(direction === "down") {
+        $('[data-nav]').addClass('fixed');
+      } else {
+        $('[data-nav]').addClass('fixed');
+      }
+    },
+    offset: 40
+  })
+  var waypoints = $('[data-main]').waypoint({
+    handler: function(direction) {
+      if(direction === "up") {
+        $('[data-nav]').removeClass('fixed');
+      }
+    }
+  })
+});
