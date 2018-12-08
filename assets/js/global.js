cca0e25accc35fe8bdcd831498271fabe6e40d09
sdaponte <stacie.daponte@gmail.com>
2018-12-08 13:37:16 -0500

    Implement styles and scripts for accordion

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,10 +1,10 @@
 $(function(){
-  
+
   // Adds current year to copyright date
 	var currentYear = (new Date()).getFullYear();
 	$("[data-year]").text(currentYear);
-  
-  
+
+
   // Table of Contents for lesson notes
   var ToC = "<ul>";
   var newLine, el, title, link;
@@ -21,15 +21,15 @@ $(function(){
 
     ToC += newLine;
   });
-  
+
   ToC +="</ul>";
   $(".table-of-contents").append(ToC);
-  
+
   $('[data-collapse]').on("click", function(){
     $(this).parent().toggleClass("collapse");
   });
-  
-  
+
+
   // fix table of contents on scroll
   var waypoints = $('[data-nav]').waypoint({
     handler: function(direction) {
@@ -48,7 +48,14 @@ $(function(){
       }
     }
   });
-  
+
   // open external links in a new window
   $("a[href^='http://'], a[href^='https://'], a[href^='/llc-digital-skills/assets/img/']").attr("target","_blank");
+
+
+  //accordion control
+  $('.summary').click(function(){
+    $(this).toggleClass('opened');
+    $(this).next().slideToggle();
+  });
 });
